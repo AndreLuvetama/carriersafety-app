3d5864c568420d3ee58384826f20eb069af902c4
AndreLuvetama <andreluares30@gmail.com>
2024-08-13 11:35:09 -0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <BrowserRouter>
           <Routes>
               <Route path='/' element={<Home/>}/>
-              <Route path='/datainfo' element={<DataInfo/>}/>
+              <Route path='/carriersafety-app' element={<DataInfo/>}/>
           </Routes>
       
       </BrowserRouter>
